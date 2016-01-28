4a361284fa6fd72b88e14ffdb0fa45e34d191535
Jacob O'Donnell <jacobodonnell@gmail.com>
2016-01-27 23:39:32 -0500

    fixed broken page

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -18,7 +18,7 @@ import 'phoenix_html';
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-// import socket from "./socket"
+import socket from './socket';  //eslint-disable-line no-unused-vars
 
 import React from 'react';
 import ReactDOM from 'react-dom';
